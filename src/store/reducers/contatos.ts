294e59a67d9a2ed67d83de47531c670b1c4c19ec
Caroline Meireles <carolinemeireles.si@gmail.com>
2025-06-10 16:18:59 -0300

    add reducers do contato

--- src/store/reducers/contatos.ts
+++ src/store/reducers/contatos.ts
@@ -14,16 +14,44 @@ const contatosSlice = createSlice({
   name: 'contatos',
   initialState,
   reducers: {
-    cadastrar: (state, action: PayloadAction<Contato>) => {
+    adicionar: (state, action: PayloadAction<Contato>) => {
       const contato = action.payload
-      if (state.itens.find((c) => c.id === contato.id)) {
-        alert('Contato já cadastrado')
+      const contatoBusca = state.itens.find((c) => c.id === contato.id)
+      if (contatoBusca) {
+        alert('Contato já adicionado')
       } else {
         state.itens.push(contato)
+      }
+    },
+    cadastrar: (state, action: PayloadAction<Omit<Contato, 'id'>>) => {
+      const contatoBusca = state.itens.find(
+        (contato) => contato.nome.toLocaleLowerCase() ===
+        action.payload.nome.toLocaleLowerCase()
+      )
+      if (contatoBusca) {
+        alert('Contato já cadastrado')
+      } else {
+        const ultimoContato = state.itens[state.itens.length - 1]
+        const contatoNovo = {
+          ...action.payload,
+          id: ultimoContato ? ultimoContato.id + 1 : 1
+        }
+        state.itens.push(contatoNovo)
+      }
+    },
+    remover: (state, action: PayloadAction<number>) => {
+      state.itens = state.itens.filter((contato) => contato.id !== action.payload)
+    },
+    editar: (state, action: PayloadAction<Contato>) => {
+      const indexContato = state.itens.findIndex(
+        (c) => c.id === action.payload.id
+      )
+      if (indexContato >= 0) {
+        state.itens[indexContato] = action.payload
       }
     }
   }
 })
 
-export const { cadastrar } = contatosSlice.actions
+export const { adicionar, cadastrar, remover, editar } = contatosSlice.actions
 export default contatosSlice.reducer
